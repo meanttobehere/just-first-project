c8838b025597eb64a5dfbba289e7466d90f36916
exon <exon@exon.ru>
2021-09-23 14:24:44 +0500

    footer progress 50%

--- src/pages/formelements/formelements.js
+++ src/pages/formelements/formelements.js
@@ -3,6 +3,7 @@ import img1 from '../../svg/uikit-logotype.svg';
 import img2 from '../../svg/arrow-right.svg';
 import img3 from '../../svg/arrow-back.svg';
 import img4 from '../../svg/arrow-forward.svg';
+import img5 from '../../svg/facebook-icon.svg';
 import '../../components/inpt/inpt.js'
 import '../../components/date-dropdown/date-dropdown.js'
 
