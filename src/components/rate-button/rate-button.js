1beb807098c69a0fcfcbcf278196550c4a6f64c6
exon <exon@exon.ru>
2021-09-28 16:00:00 +0500

    rate-button added

---
+++ src/components/rate-button/rate-button.js
@@ -0,0 +1,2 @@
+import rateButton__star from './rate-button__star.svg'
+import rateButton__starBorder from './rate-button__star-border.svg'
--- src/pages/formelements/formelements.js
+++ src/pages/formelements/formelements.js
@@ -9,4 +9,5 @@ import '../../components/date-dropdown/date-dropdown.js'
 import '../../components/checkbox-list/checkbox-list.js'
 import '../../components/range-slider/range-slider.js'
 import '../../components/pagination/pagination.js'
+import '../../components/rate-button/rate-button.js'
 
