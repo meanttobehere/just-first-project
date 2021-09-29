ece24154a774d094a68d3c987d77478a54d6cc42
exon <exon@exon.ru>
2021-09-29 17:26:39 +0500

    review added

--- src/pages/formelements/formelements.js
+++ src/pages/formelements/formelements.js
@@ -4,6 +4,7 @@ import img2 from '../../images/arrow-right.svg';
 import img3 from '../../images/arrow-back.svg';
 import img4 from '../../images/arrow-forward.svg';
 import img5 from '../../images/facebook-icon.svg';
+import photo from './images/photo.jpg';
 import '../../components/inpt/inpt.js'
 import '../../components/date-dropdown/date-dropdown.js'
 import '../../components/checkbox-list/checkbox-list.js'
