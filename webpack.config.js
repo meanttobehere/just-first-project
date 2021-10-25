3f43da5378b640e0fa2e5d8acdb21304c9799805
exon <exon@exon.ru>
2021-10-26 00:04:19 +0500

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-/*const MiniCssExtractPlugin = require("mini-css-extract-plugin")*/
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const path = require('path')
-const fs = require('fs')
-
-const htmlPlugins = generateHtmlPlugins();
-//const cssPlugins = new MiniCssExtractPlugin({filename: ({ chunk }) => `${chunk.name.replace('/js/', '/css/')}.css`,})
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {    
     entry: {
@@ -14,53 +10,59 @@ module.exports = {
         colorstype: path.resolve(__dirname, './src/pages/colorstype/colorstype.js'),
         cards: path.resolve(__dirname, './src/pages/cards/cards.js'),
         headersfooters: path.resolve(__dirname, './src/pages/headersfooters/headersfooters.js'),
+        searchpage: path.resolve(__dirname, './src/pages/searchpage/searchpage.js'),
     },
 
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist/toxinpages'),
+        filename: 'bundles/[name].bundle.js',
     },
 
     module: {
         rules: [
             {
+                test: /\.(woff(2)?|ttf|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: './fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(jpe?g|svg|png)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: './images/[name][ext]',
+                },
+            },
+            {
                 test: /\.pug$/,
-                use: 'pug-loader'
+                use: 'pug-loader',
             },            
             { 
                 test: /\.s[ac]ss$/i, 
-                use: [/*MiniCssExtractPlugin.loader,*/"style-loader", "css-loader", /*"resolve-url-loader",*/ "sass-loader"],
-            },  
-            {   test: /\.(jpe?g|svg|woff|ttf)$/, 
-                loader: "file-loader",
-                options: {
-                    name: '/images/[name].[ext]',
-                    useRelativePaths: true,
-                  }
-            }         
+                use: [
+                    {
+                        loader: "style-loader",                        
+                    },
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: true },
+                    },                 
+                    {
+                        loader: 'resolve-url-loader',                        
+                    },
+                    {                    
+                        loader: 'sass-loader',
+                        options: { sourceMap: true },
+                    }
+                ]                    
+            },                       
         ],
     },
-/*
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                style: {
-                    name: 'style',
-                    test: /style\.s?css$/,
-                    chunks: 'all',
-                    enforce: true,
-                },
-                editor: {
-                    name: 'editor',
-                    test: /editor\.s?css$/,
-                    chunks: 'all',
-                    enforce: true,
-                },
-            },
-        },
-    },
-*/
-    plugins: [/*cssPlugins,*/ ...htmlPlugins, new CleanWebpackPlugin()],
+    plugins: [
+        ...generateHtmlPlugins(), 
+        new CleanWebpackPlugin()                         
+    ],
 }
 
 function findByExtension(directoryPath, extension)
@@ -85,16 +87,14 @@ function findByExtension(directoryPath, extension)
 }
 
 function generateHtmlPlugins() {    
-    let items = findByExtension(path.resolve(__dirname,'./src/pages/'), 'pug');
-    console.log(items);
+    let items = findByExtension(path.resolve(__dirname,'./src/pages/'), 'pug');    
 
     return items.map((item, id) => {
         let name = path.basename(item).split('.')[0];
         return new HtmlWebpackPlugin({            
             filename: `${name}.html`,
-            chunks : [name],              // inject only the same name bundle
-            template: item,
-           // inject: false,
+            chunks : [name],
+            template: item,           
         })
     })
 }
