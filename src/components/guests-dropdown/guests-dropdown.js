5bd917deffa11c0b7b191ae17f1a4380524a65ad
exon <exon@exon.ru>
2021-10-26 23:57:42 +0500

    guest-dropdown refactoring

--- src/components/guests-dropdown/guests-dropdown.js
+++ src/components/guests-dropdown/guests-dropdown.js
@@ -1,57 +1,45 @@
-import '../dropdown/dropdown.js'
+import '../dropdown/dropdown';
+import { dropdownItemGetCounter } from '../dropdown/__item/dropdown__item';
 
-function guestsDropdownsInit()
-{
-    let dropdowns = document.getElementsByClassName('guests-dropdown');
-    
-    function generateText(adults, children, babies)
-    {
-        let text = '';
+function generateText(adults, children, babies) {
+  let text = '';
 
-        let guests = adults + children + babies;
+  const guests = adults + children + babies;
 
-        if (guests === 0)
-            return ("Сколько гостей");
+  if (guests === 0) { return ('Сколько гостей'); }
 
-        if (guests % 10 === 1 && guests !== 11)
-            text = guests + ' гость';
-        else if (guests % 10 >= 2 && guests % 10 <= 4)
-            text = guests + ' гостя';
-        else
-            text = guests + ' гостей';
+  if (guests % 10 === 1 && guests !== 11) { text = `${guests} гость`; } else if (guests % 10 >= 2 && guests % 10 <= 4) { text = `${guests} гостя`; } else { text = `${guests} гостей`; }
 
-        if (babies === 0)
-            return text;
+  if (babies === 0) { return text; }
 
-        if (babies % 10 === 1 && babies !== 11)
-            text += ', ' + babies + ' младенец';
-        else if (babies % 10 >= 2 && babies % 10 <= 4)
-            text += ', ' + babies + ' младенца';
-        else
-            text += ', ' + babies + ' младенцев';
+  if (babies % 10 === 1 && babies !== 11) { text += `, ${babies} младенец`; } else if (babies % 10 >= 2 && babies % 10 <= 4) { text += `, ${babies} младенца`; } else { text += `, ${babies} младенцев`; }
 
-        return text;
-    }    
-    
-    for (let i = 0; i < dropdowns.length; i++)
-    {        
-        let text = dropdowns[i].querySelector(".dropdown__text");
-        let items = dropdowns[i].getElementsByClassName("dropdown__item-counter");
-        let menu = dropdowns[i].querySelector(".dropdown__menu");
+  return text;
+}
 
-        function setText(){
-            let adults = Number.parseInt(items[0].textContent);
-            let children = Number.parseInt(items[1].textContent);
-            let babies = Number.parseInt(items[2].textContent);
-            text.textContent = generateText(adults, children, babies);
-        }
+function guestsDropdownsInit() {
+  const dropdowns = document.getElementsByClassName('js-guests-dropdown');
 
-        menu.addEventListener("click", () => {
-            setText();
-        });
+  for (let i = 0; i < dropdowns.length; i += 1) {
+    const dropdown = dropdowns[i];
 
-        setText();
-    }
+    const field = dropdown.querySelector('.js-dropdown__field');
+    const [adults, children, babies] = dropdown.getElementsByClassName('js-dropdown-item');
+    const menu = dropdown.querySelector('.js-dropdown__menu');
+
+    const setText = function setText() {
+      const adultsCount = dropdownItemGetCounter(adults);
+      const childrenCount = dropdownItemGetCounter(children);
+      const babiesCount = dropdownItemGetCounter(babies);
+      field.textContent = generateText(adultsCount, childrenCount, babiesCount);
+    };
+
+    menu.addEventListener('click', () => {
+      setText();
+    });
+
+    setText();
+  }
 }
 
 guestsDropdownsInit();
