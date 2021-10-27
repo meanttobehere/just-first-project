5d0d90b148e8eac312b38393391a8c75409e2503
exon <exon@exon.ru>
2021-10-27 17:43:58 +0500

    footer refactoring

---
+++ src/components/footer/footer.js
@@ -0,0 +1,21 @@
+import '../inpt/inpt';
+
+function footersInit() {
+  const footers = document.getElementsByClassName('js-footer');
+
+  for (let i = 0; i < footers.length; i += 1) {
+    const groups = footers[i].getElementsByClassName('js-footer__group');
+
+    for (let j = 0; j < groups.length; j += 1) {
+      const group = groups[j];
+      const title = group.querySelector('.js-footer__group-title');
+
+      title.onclick = () => {
+        group.classList.toggle('footer__group_open');
+        if (group.classList.contains('footer__group_open')) { group.style.maxHeight = `${group.scrollHeight}px`; } else { group.style.maxHeight = '16px'; }
+      };
+    }
+  }
+}
+
+footersInit();
