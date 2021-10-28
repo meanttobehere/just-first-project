e56bf4779cc1ef4f5c527140bba47f927b5abb42
exon <exon@exon.ru>
2021-10-28 16:01:14 +0500

    search page added

--- src/pages/searchpage/searchpage.js
+++ src/pages/searchpage/searchpage.js
@@ -1,8 +1,19 @@
 import './searchpage.scss';
-import '../../components/header/header.js'
-import '../../components/footer/footer.js'
-import '../../components/guests-dropdown/guests-dropdown.js'
-import '../../components/dropdown/dropdown.js'
-import '../../components/range-slider/range-slider.js'
-import '../../components/checkbox-list/checkbox-list.js'
-import '../../components/room-dropdown/room-dropdown.js'
+import '../../components/header/header.js';
+import '../../components/footer/footer.js';
+import '../../components/guests-dropdown/guests-dropdown.js';
+import '../../components/dropdown/dropdown.js';
+import '../../components/range-slider/range-slider.js';
+import '../../components/checkbox-list/checkbox-list.js';
+import '../../components/room-dropdown/room-dropdown.js';
+
+function searchPageInit(){
+    const filterControl = document.querySelector('.js-filter-control');
+    const filterContainer = document.querySelector('.js-filter-container');
+
+    filterControl.addEventListener('click', () => {
+        filterContainer.classList.toggle('filter-container_open');
+    })
+}
+
+searchPageInit();
