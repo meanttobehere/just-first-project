07c007848352a32b524ebe484aea8d207398077a
exon <exon@exon.ru>
2021-10-29 18:18:10 +0500

    header burger menu added, mobile version font size inreased

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,13 +12,18 @@ function headersInit() {
 
     menu.addEventListener('click', () => {
       navbar.classList.toggle('header__navbar_open');
+      menu.children[0].classList.toggle('hamburger_active');
     });
 
     const changeHeader = (isDesktopView) => {
       if (isDesktopView) {
         headers[i].appendChild(userblock);
+        const [button1, button2] = userblock.getElementsByClassName('button');
+        [button1, button2].forEach((button) => {button.classList.remove('button_size-xl')});       
       } else {
         navbar.appendChild(userblock);
+        const [button1, button2] = userblock.getElementsByClassName('button');
+        [button1, button2].forEach((button) => {button.classList.add('button_size-xl')}); 
       }
     };
 
