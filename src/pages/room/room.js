d7437a3de2c8ed34120179a76f69c2a20278f41e
exon <exon@exon.ru>
2021-11-02 18:06:41 +0500

    room page added

---
+++ src/pages/room/room.js
@@ -0,0 +1,16 @@
+import './room.scss';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+import '../../components/guests-dropdown/guests-dropdown';
+import '../../components/dropdown/dropdown';
+import '../../components/room-dropdown/room-dropdown';
+import '../../components/room-card/room-card';
+import '../../components/date-dropdown/date-dropdown';
+
+import './room/photo1.png';
+import './room/photo2.png';
+import './room/photo3.png';
+
+import './reviews/photo1.jpg';
+import './reviews/photo2.jpg';
