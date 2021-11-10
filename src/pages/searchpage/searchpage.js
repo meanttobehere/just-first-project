eb66671a063ff5391ff6d0985d3bdfac197c2336
exon <exon@exon.ru>
2021-11-10 14:00:06 +0500

    searchpage updated

--- src/pages/searchpage/searchpage.js
+++ src/pages/searchpage/searchpage.js
@@ -1,23 +1,46 @@
 import './searchpage.scss';
-import '../../components/header/header.js';
-import '../../components/footer/footer.js';
-import '../../components/guests-dropdown/guests-dropdown.js';
-import '../../components/dropdown/dropdown.js';
-import '../../components/range-slider/range-slider.js';
-import '../../components/room-dropdown/room-dropdown.js';
-import '../../components/room-card/room-card.js';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
 import '../../components/date-dropdown/date-dropdown';
+import '../../components/guests-dropdown/guests-dropdown';
+import '../../components/range-slider/range-slider';
+import '../../components/room-dropdown/room-dropdown';
+import '../../components/checkbox-list/checkbox-list';
+import '../../components/room-card/room-card';
 
-import './rooms/room1.png';
-import './rooms/room2.png';
+import '../../assets/images/room_photo1.jpg';
+import '../../assets/images/room_photo2.jpg';
+import '../../assets/images/room_photo3.jpg';
+import '../../assets/images/room_photo4.jpg';
+import '../../assets/images/room_photo5.jpg';
+import '../../assets/images/room_photo6.jpg';
+import '../../assets/images/room_photo7.jpg';
+import '../../assets/images/room_photo8.jpg';
+import '../../assets/images/room_photo9.jpg';
+import '../../assets/images/room_photo10.jpg';
+import '../../assets/images/room_photo11.jpg';
+import '../../assets/images/room_photo12.jpg';
 
-function searchPageInit(){
-    const filterControl = document.querySelector('.js-filter-control');
-    const filterContainer = document.querySelector('.js-filter-container');
+function searchPageInit() {
+  const filterControl = document.querySelector('.js-filter-control');
+  const filterContainer = document.querySelector('.js-filter-container');
+  const footer = document.querySelector('.js-footer');
+  const data = document.querySelector('.js-data-container');
 
-    filterControl.addEventListener('click', () => {
-        filterContainer.classList.toggle('filter-container_open');
-    })
+  let stateIsOpen = true;
+  filterControl.addEventListener('click', () => {
+    filterContainer.classList.toggle('filter-container_open');
+    if (stateIsOpen) {
+      footer.style.display = 'none';
+      data.style.display = 'none';
+      window.scrollTo(0, 0);
+    } else {
+      footer.style.display = 'block';
+      data.style.display = 'flex';
+    }
+    stateIsOpen = !stateIsOpen;
+  });
 }
 
 searchPageInit();
