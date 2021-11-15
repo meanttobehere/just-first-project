a980eccf373c5f49cab3e3d35ef94d75c2465c27
exon <exon@exon.ru>
2021-11-15 20:05:49 +0500

    checkbox-list component updated

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,13 +1,17 @@
-function chechboxListsInit() {
-  const checkboxLists = document.getElementsByClassName('js-checkbox-list checkbox-list_expandable');
+class CheckboxList{
+  #list;
 
-  for (let i = 0; i < checkboxLists.length; i += 1) {
-    const title = checkboxLists[i].querySelector('.js-checkbox-list__title');
+  constructor(list){
+    this.#list = list;
+    const title = list.querySelector('.js-checkbox-list__title');
+    title.onclick = this._handleTitleClick.bind(this);
+  }
 
-    title.onclick = function titleClickHandler() {
-      checkboxLists[i].classList.toggle('checkbox-list_open');
-    };
+  _handleTitleClick(){
+    this.#list.classList.toggle('checkbox-list_open');
   }
 }
 
-chechboxListsInit();
+document.querySelectorAll('.js-checkbox-list.checkbox-list_expandable').forEach((list) => {
+  list._checkboxList = new CheckboxList(list);
+});
