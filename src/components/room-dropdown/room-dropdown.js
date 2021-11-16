1f44bb7388d2890c27bb7984e75e2a6dc7ca431e
exon <exon@exon.ru>
2021-11-16 16:01:29 +0500

    room-dropdown component updated

--- src/components/room-dropdown/room-dropdown.js
+++ src/components/room-dropdown/room-dropdown.js
@@ -1,38 +1,40 @@
 import '../dropdown/dropdown';
-import { dropdownItemGetCounter } from '../dropdown/__item/dropdown__item';
 
-function generateText(bedrooms, beds) {
-  let text = '';
+class RoomDropdown{
+  #dropdown;
 
-  if (bedrooms % 10 === 1 && bedrooms !== 11) { text = `${bedrooms} спальня`; } else if (bedrooms % 10 >= 2 && bedrooms % 10 <= 4) { text = `${bedrooms} спальни`; } else { text = `${bedrooms} спален`; }
+  constructor(roomDropdown){
+    this.#dropdown = roomDropdown.querySelector('.js-dropdown')._dropdown;
+    roomDropdown
+      .querySelector('.js-dropdown__menu')
+      .addEventListener('click', this._handleMenuClick.bind(this));
+  }
 
-  if (beds === 0) { return text; }
+  _handleMenuClick(){
+    const [
+      bedrooms,
+      beds
+    ] = this.#dropdown.getItems().map(item => item.getCounterValue());
+    this.#dropdown.setText(this._generateText(bedrooms, beds));
+  }
 
-  if (beds % 10 === 1 && beds !== 11) { text += `, ${beds} кровать...`; } else if (beds % 10 >= 2 && beds % 10 <= 4) { text += `, ${beds} кровати...`; } else { text += `, ${beds} кроватей...`; }
+  _generateText(bedrooms, beds){
+    let text = '';
 
-  return text;
-}
+    if (bedrooms % 10 === 1 && bedrooms !== 11) { text = `${bedrooms} спальня`; }
+    else if (bedrooms % 10 >= 2 && bedrooms % 10 <= 4) { text = `${bedrooms} спальни`; }
+    else { text = `${bedrooms} спален`; }
 
-function roomDropdownsInit() {
-  const dropdowns = document.getElementsByClassName('js-room-dropdown');
+    if (beds === 0) { return text; }
 
-  for (let i = 0; i < dropdowns.length; i += 1) {
-    const field = dropdowns[i].querySelector('.js-dropdown__field').childNodes[0];
-    const [bedrooms, beds] = dropdowns[i].getElementsByClassName('js-dropdown-item');
-    const menu = dropdowns[i].querySelector('.js-dropdown__menu');
+    if (beds % 10 === 1 && beds !== 11) { text += `, ${beds} кровать...`; }
+    else if (beds % 10 >= 2 && beds % 10 <= 4) { text += `, ${beds} кровати...`; }
+    else { text += `, ${beds} кроватей...`; }
 
-    const setText = function setText() {
-      const bedroomsCount = dropdownItemGetCounter(bedrooms);
-      const bedsCount = dropdownItemGetCounter(beds);
-      field.textContent = generateText(bedroomsCount, bedsCount);
-    };
-
-    menu.addEventListener('click', () => {
-      setText();
-    });
-
-    setText();
+    return text;
   }
 }
 
-roomDropdownsInit();
+document.querySelectorAll('.js-room-dropdown').forEach(dropdown => {
+  dropdown._roomDropdown = new RoomDropdown(dropdown);
+});
