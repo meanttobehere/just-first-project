f6beb1814ca10056e368b7b63a41e76f96fba189
exon <exon@exon.ru>
2021-11-16 11:04:40 +0500

    dropdown component updated

--- src/components/dropdown/__item/dropdown__item.js
+++ src/components/dropdown/__item/dropdown__item.js
@@ -1,57 +1,45 @@
-function getCounterDownValue(counter) {
-  let currentValue = Number.parseInt(counter.textContent, 10);
-  if (currentValue === 0) { return 0; }
-  currentValue -= 1;
-  return currentValue;
-}
+class DropdownItem{
+  #minusButton;
+  #plusButton;
+  #counter;
 
-function getCounterUpValue(counter) {
-  let currentValue = Number.parseInt(counter.textContent, 10);
-  currentValue += 1;
-  return currentValue;
-}
+  constructor(item){
+    [this.#minusButton, this.#plusButton] = item.querySelectorAll('.js-dropdown-item__button');
+    this.#counter = item.querySelector('.js-dropdown-item__counter');
 
-function getMinusButtonStyle(button, counter) {
-  const { classList } = button;
-  if (counter.textContent === '0') {
-    classList.add('dropdown-item__button_disable');
-  } else {
-    classList.remove('dropdown-item__button_disable');
+    this.#minusButton.onclick = this._handleMinusButtonClick.bind(this);
+    this.#plusButton.onclick = this._handlePlusButtonClick.bind(this);
   }
-  return classList;
-}
 
-function dropdownItemsInit() {
-  const items = document.getElementsByClassName('js-dropdown-item');
+  getCounterValue(){
+    return Number.parseInt(this.#counter.textContent, 10);
+  }
 
-  for (let i = 0; i < items.length; i += 1) {
-    const [minusButton, plusButton] = items[i].getElementsByClassName('js-dropdown-item__button');
-    const counter = items[i].querySelector('.js-dropdown-item__counter');
+  reset(){
+    this.#counter.textContent = '0';
+    this._updateMinusButtonStyle();
+  }
 
-    minusButton.classList = getMinusButtonStyle(minusButton, counter);
+  _handleMinusButtonClick(){
+    if (this.getCounterValue() === 0) { return; }
+    this.#counter.textContent = this.getCounterValue() - 1;
+    this._updateMinusButtonStyle();
+  }
 
-    minusButton.addEventListener('click', () => {
-      counter.textContent = getCounterDownValue(counter);
-      minusButton.classList = getMinusButtonStyle(minusButton, counter);
-    });
+  _handlePlusButtonClick(){
+    this.#counter.textContent = this.getCounterValue() + 1;
+    this._updateMinusButtonStyle();
+  }
 
-    plusButton.addEventListener('click', () => {
-      counter.textContent = getCounterUpValue(counter);
-      minusButton.classList = getMinusButtonStyle(minusButton, counter);
-    });
+  _updateMinusButtonStyle() {
+    if (this.getCounterValue() === 0) {
+      this.#minusButton.classList.add('dropdown-item__button_disable');
+    } else {
+      this.#minusButton.classList.remove('dropdown-item__button_disable');
+    }
   }
 }
 
-dropdownItemsInit();
-
-export function dropdownItemReset(item) {
-  const [minusButton] = item.getElementsByClassName('js-dropdown-item__button');
-  const counter = item.querySelector('.js-dropdown-item__counter');
-  counter.textContent = '0';
-  minusButton.classList = getMinusButtonStyle(minusButton, counter);
-}
-
-export function dropdownItemGetCounter(item) {
-  const counter = item.querySelector('.js-dropdown-item__counter');
-  return Number.parseInt(counter.textContent, 10);
-}
+document.querySelectorAll('.js-dropdown-item').forEach(item => {
+  item._dropdownItem = new DropdownItem(item);
+});
--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,56 +1,87 @@
-import { dropdownItemReset, dropdownItemGetCounter } from './__item/dropdown__item';
+import './__item/dropdown__item';
 
-function getClearButtonStyle(button, items) {
-  const { classList } = button;
-  let sum = 0;
-  for (let j = 0; j < items.length; j += 1) { sum += dropdownItemGetCounter(items[j]); }
-  if (sum === 0) { classList.add('button_hide'); } else { classList.remove('button_hide'); }
-  return classList;
-}
+class Dropdown{
+  #dropdown;
+  #clearButton;
+  #acceptButton;
+  #menu;
+  #items;
+  #field;
 
-function dropdownsInit() {
+  constructor(dropdown){
+    this.#dropdown = dropdown;
+    this.#menu = dropdown.querySelector('.js-dropdown__menu');
+    this.#field = dropdown.querySelector('.js-dropdown__field');
+    [...this.#items] = dropdown.querySelectorAll('.js-dropdown-item');
 
-  document.addEventListener('click', (event) => {
-    const dropdownField = event.target.closest('.js-dropdown__field');
-    const currentDropdown = event.target.closest('.js-dropdown');
+    this.#menu.onclick = this._handleMenuClick.bind(this);
+    this.#field.onclick = this._handleFieldClick.bind(this);
 
-    if (!dropdownField && currentDropdown) { return; }
-
-    if (dropdownField) { currentDropdown.classList.toggle('dropdown_active'); }
-
-    document.querySelectorAll('.js-dropdown.dropdown_active').forEach((dropdown) => {
-      if (dropdown === currentDropdown) { return; }
-      dropdown.classList.remove('dropdown_active');
-    });
-  });
-
-  const dropdowns = document.getElementsByClassName('js-dropdown');
-
-  for (let i = 0; i < dropdowns.length; i += 1) {
-    const dropdown = dropdowns[i];
-    const dropdownButtonsContainer = dropdown.querySelector('.js-dropdown__buttons-container');    
-
-    if (dropdownButtonsContainer) {
-      const [clearBtn, acceptBtn] = dropdownButtonsContainer.children;
-      const items = dropdown.getElementsByClassName('js-dropdown-item');
-      const menu = dropdown.querySelector('.js-dropdown__menu');
-
-      clearBtn.addEventListener('click', () => {
-        dropdown.classList.remove('dropdown_active');
-        for (let j = 0; j < items.length; j += 1) { dropdownItemReset(items[j]); }
-      });
-
-      acceptBtn.addEventListener('click', () => {
-        dropdown.classList.remove('dropdown_active');
-      });
-
-      menu.addEventListener('click', () => {
-        clearBtn.classList = getClearButtonStyle(clearBtn, items);
-      });
-
-      clearBtn.classList = getClearButtonStyle(clearBtn, items);
+    const buttonsContainer = dropdown.querySelector('.js-dropdown__buttons-container');
+    if (buttonsContainer){
+      [this.#clearButton, this.#acceptButton] = buttonsContainer.children;
+      this.#clearButton.onclick = this._handleClearButtonClick.bind(this);
+      this.#acceptButton.onclick = this._handleAcceptButtonClick.bind(this);
+      this._updateClearButtonStyle();
     }
+  }
+
+  _close(){
+    this.#dropdown.classList.remove('dropdown_active');
+  }
+
+  _open(){
+    this.#dropdown.classList.add('dropdown_active');
+  }
+
+  _updateClearButtonStyle(){
+    const sum = this.#items
+      .map(item => item._dropdownItem.getCounterValue())
+      .reduce((val1, val2) => val1 + val2);
+ 
+    if (sum === 0) { this.#clearButton.classList.add('button_hide'); } 
+    else { this.#clearButton.classList.remove('button_hide'); }
+  }
+
+  _handleClearButtonClick(){
+    this.#items.forEach(item => { item._dropdownItem.reset(); });
+    this._close();
+  }
+
+  _handleAcceptButtonClick(){
+    this._close();
+  }
+
+  _handleMenuClick(){
+    if (this.#clearButton){
+      this._updateClearButtonStyle();
+    }
+  }
+
+  _handleFieldClick(event){
+    const dropdownIsNotActive = this.#dropdown.classList.contains('dropdown_active') === false;
+
+    if (dropdownIsNotActive){
+      this._open();
+      document.addEventListener('click', this._createDocumentClickHandler());
+      event.stopPropagation();
+    }
+  }
+
+  _createDocumentClickHandler(){
+    const handleDocumentClick = (event) => {
+      const dropdown = event.target.closest('.js-dropdown');
+      const isField = event.target.closest('.js-dropdown__field');
+      if (dropdown !== this.#dropdown
+        || dropdown === this.#dropdown && isField){
+        this._close();
+        document.removeEventListener('click', handleDocumentClick);
+      }
+    }
+    return handleDocumentClick;
   }
 }
 
-dropdownsInit();
+document.querySelectorAll('.js-dropdown').forEach(dropdown => {
+  dropdown._dropdown = new Dropdown(dropdown);
+});
