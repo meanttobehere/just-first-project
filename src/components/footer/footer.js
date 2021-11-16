13ecc980faf1c9529d4bddcbf9a7fea59d9f02f3
exon <exon@exon.ru>
2021-11-16 12:18:43 +0500

    footer component updated

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,19 +1,22 @@
-function footersInit() {
-  const footers = document.getElementsByClassName('js-footer');
+class Footer{
+  constructor(footer){
+    const groups = footer.querySelectorAll('.js-footer__group');
+    groups.forEach(group => {
+      group.querySelector('.js-footer__group-title').onclick = this._handleTitleClick.bind(group);
+    });
+  }
 
-  for (let i = 0; i < footers.length; i += 1) {
-    const groups = footers[i].getElementsByClassName('js-footer__group');
-
-    for (let j = 0; j < groups.length; j += 1) {
-      const group = groups[j];
-      const title = group.querySelector('.js-footer__group-title');
-
-      title.onclick = () => {
-        group.classList.toggle('footer__group_open');
-        if (group.classList.contains('footer__group_open')) { group.style.maxHeight = `${group.scrollHeight}px`; } else { group.style.maxHeight = '16px'; }
-      };
+  _handleTitleClick(){
+    const group = this;
+    group.classList.toggle('footer__group_open');
+    if (group.classList.contains('footer__group_open')) { 
+      group.style.maxHeight = `${group.scrollHeight}px`; 
+    } else { 
+      group.style.maxHeight = '16px'; 
     }
   }
 }
 
-footersInit();
+document.querySelectorAll('.js-footer').forEach(footer => {
+  footer._footer = new Footer(footer);
+})
