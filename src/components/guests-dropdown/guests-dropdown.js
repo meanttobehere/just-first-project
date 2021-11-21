f7ef39638cb672f7b0fdb28b342978e0c8c6bdac
exon <exon@exon.ru>
2021-11-21 15:57:30 +0500

    guest-dropdown component eslint fix

--- src/components/guests-dropdown/guests-dropdown.js
+++ src/components/guests-dropdown/guests-dropdown.js
@@ -4,37 +4,50 @@ class GuestsDropdown {
   #dropdown;
 
   constructor(guestsDropdown) {
-    this.#dropdown = guestsDropdown.querySelector('.js-dropdown')._dropdown;
+    this.#dropdown = guestsDropdown.querySelector('.js-dropdown').dropdown;
     guestsDropdown
       .querySelector('.js-dropdown__menu')
-      .addEventListener('click', this._handleMenuClick.bind(this));
+      .addEventListener('click', this.#handleMenuClick.bind(this));
   }
 
-  _handleMenuClick() {
+  #handleMenuClick() {
     const [
       adults,
       children,
       babies,
     ] = this.#dropdown.getItems().map((item) => item.getCounterValue());
-    this.#dropdown.setText(this._generateText(adults, children, babies));
+    this.#dropdown.setText(GuestsDropdown.#genText(adults, children, babies));
   }
 
-  _generateText(adults, children, babies) {
+  static #genText(adults, children, babies) {
     let text = '';
     const guests = adults + children + babies;
 
     if (guests === 0) { return ('Сколько гостей'); }
 
-    if (guests % 10 === 1 && guests !== 11) { text = `${guests} гость`; } else if (guests % 10 >= 2 && guests % 10 <= 4) { text = `${guests} гостя`; } else { text = `${guests} гостей`; }
+    if (guests % 10 === 1 && guests !== 11) {
+      text = `${guests} гость`;
+    } else if (guests % 10 >= 2 && guests % 10 <= 4) {
+      text = `${guests} гостя`;
+    } else {
+      text = `${guests} гостей`;
+    }
 
     if (babies === 0) { return text; }
 
-    if (babies % 10 === 1 && babies !== 11) { text += `, ${babies} младенец`; } else if (babies % 10 >= 2 && babies % 10 <= 4) { text += `, ${babies} младенца`; } else { text += `, ${babies} младенцев`; }
+    if (babies % 10 === 1 && babies !== 11) {
+      text += `, ${babies} младенец`;
+    } else if (babies % 10 >= 2 && babies % 10 <= 4) {
+      text += `, ${babies} младенца`;
+    } else {
+      text += `, ${babies} младенцев`;
+    }
 
     return text;
   }
 }
 
-document.querySelectorAll('.js-guests-dropdown').forEach((dropdown) => {
-  dropdown._guestsDropdown = new GuestsDropdown(dropdown);
+document.querySelectorAll('.js-guests-dropdown').forEach((guestsDropdown) => {
+  const guestsDropdownDOM = guestsDropdown;
+  guestsDropdownDOM.guestsDropdown = new GuestsDropdown(guestsDropdown);
 });
