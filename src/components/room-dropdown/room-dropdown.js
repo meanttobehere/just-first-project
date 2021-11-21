12c1e95448b382cdd7f727932b3418a4609090e7
exon <exon@exon.ru>
2021-11-21 16:18:18 +0500

    room-dropdown eslint fix

--- src/components/room-dropdown/room-dropdown.js
+++ src/components/room-dropdown/room-dropdown.js
@@ -4,33 +4,46 @@ class RoomDropdown {
   #dropdown;
 
   constructor(roomDropdown) {
-    this.#dropdown = roomDropdown.querySelector('.js-dropdown')._dropdown;
+    this.#dropdown = roomDropdown.querySelector('.js-dropdown').dropdown;
     roomDropdown
       .querySelector('.js-dropdown__menu')
-      .addEventListener('click', this._handleMenuClick.bind(this));
+      .addEventListener('click', this.#handleMenuClick.bind(this));
   }
 
-  _handleMenuClick() {
+  #handleMenuClick() {
     const [
       bedrooms,
       beds,
     ] = this.#dropdown.getItems().map((item) => item.getCounterValue());
-    this.#dropdown.setText(this._generateText(bedrooms, beds));
+    this.#dropdown.setText(RoomDropdown.#generateText(bedrooms, beds));
   }
 
-  _generateText(bedrooms, beds) {
+  static #generateText(bedrooms, beds) {
     let text = '';
 
-    if (bedrooms % 10 === 1 && bedrooms !== 11) { text = `${bedrooms} спальня`; } else if (bedrooms % 10 >= 2 && bedrooms % 10 <= 4) { text = `${bedrooms} спальни`; } else { text = `${bedrooms} спален`; }
+    if (bedrooms % 10 === 1 && bedrooms !== 11) {
+      text = `${bedrooms} спальня`;
+    } else if (bedrooms % 10 >= 2 && bedrooms % 10 <= 4) {
+      text = `${bedrooms} спальни`;
+    } else {
+      text = `${bedrooms} спален`;
+    }
 
     if (beds === 0) { return text; }
 
-    if (beds % 10 === 1 && beds !== 11) { text += `, ${beds} кровать...`; } else if (beds % 10 >= 2 && beds % 10 <= 4) { text += `, ${beds} кровати...`; } else { text += `, ${beds} кроватей...`; }
+    if (beds % 10 === 1 && beds !== 11) {
+      text += `, ${beds} кровать...`;
+    } else if (beds % 10 >= 2 && beds % 10 <= 4) {
+      text += `, ${beds} кровати...`;
+    } else {
+      text += `, ${beds} кроватей...`;
+    }
 
     return text;
   }
 }
 
-document.querySelectorAll('.js-room-dropdown').forEach((dropdown) => {
-  dropdown._roomDropdown = new RoomDropdown(dropdown);
+document.querySelectorAll('.js-room-dropdown').forEach((roomDropdown) => {
+  const roomDropdownDOM = roomDropdown;
+  roomDropdownDOM.roomDropdown = new RoomDropdown(roomDropdown);
 });
