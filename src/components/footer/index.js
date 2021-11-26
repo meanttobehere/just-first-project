da34c5cb939f2033cb14f8910a27b53cd1897dbf
exon <exon@exon.ru>
2021-11-26 16:09:30 +0500

    issue #2: footer updated

--- src/components/footer/index.js
+++
@@ -1,6 +0,0 @@
-import Footer from './Footer';
-
-document.querySelectorAll('.js-footer').forEach((footer) => {
-  const footerDOM = footer;
-  footerDOM.footer = new Footer(footer);
-});
--- src/pages/headers-footers/index.js
+++
@@ -1,3 +0,0 @@
-import '../../components/header/index';
-import '../../components/footer/index';
-import './headers-footers.scss';
