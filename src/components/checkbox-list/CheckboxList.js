25579cd9c12c9be1c08ac47d979bf3e5686bb274
exon <exon@exon.ru>
2021-12-03 16:23:42 +0500

    issue #2: checkbox-list is closed by clicking outside

--- src/components/checkbox-list/CheckboxList.js
+++ src/components/checkbox-list/CheckboxList.js
@@ -1,15 +1,44 @@
 class CheckboxList {
   #list;
 
+  #title;
+
   constructor(list) {
     this.#list = list;
-    const title = list.querySelector('.js-checkbox-list__title');
-    title.onclick = this.#handleTitleClick.bind(this);
+    this.#title = list.querySelector('.js-checkbox-list__title');
+    this.#init();
+  }
+
+  #init() {
+    this.#title.onclick = this.#handleTitleClick.bind(this);
+    if (this.#isOpen) {
+      document.addEventListener('click', this.#handleDocumentClick);
+    }
   }
 
   #handleTitleClick() {
-    this.#list.classList.toggle('checkbox-list_open');
+    if (this.#isOpen) { this.#close(); } else { this.#open(); }
   }
+
+  get #isOpen() {
+    return this.#list.classList.contains('checkbox-list_open');
+  }
+
+  #open() {
+    this.#list.classList.add('checkbox-list_open');
+    document.addEventListener('click', this.#handleDocumentClick);
+  }
+
+  #close() {
+    this.#list.classList.remove('checkbox-list_open');
+    document.removeEventListener('click', this.#handleDocumentClick);
+  }
+
+  #handleDocumentClick = (event) => {
+    const list = event.target.closest('.js-checkbox-list');
+    const clickWasOutside = list !== this.#list;
+    if (clickWasOutside) { this.#close(); }
+  };
 }
 
 export default CheckboxList;
